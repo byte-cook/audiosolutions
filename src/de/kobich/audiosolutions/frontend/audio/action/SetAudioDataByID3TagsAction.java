cd5d9cbc31d20b17bccdcd0554647bccd62cd9e8
bytecook <bytecook@t-online.de>
2024-04-04 20:57:13 +0200

    add dialog to select ID3 tags for addAudioDataByID3Tags

--- src/de/kobich/audiosolutions/frontend/audio/action/SetAudioDataByID3TagsAction.java
+++ src/de/kobich/audiosolutions/frontend/audio/action/SetAudioDataByID3TagsAction.java
@@ -1,19 +1,27 @@
 package de.kobich.audiosolutions.frontend.audio.action;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.dialogs.ListSelectionDialog;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import de.kobich.audiosolutions.core.AudioSolutions;
+import de.kobich.audiosolutions.core.service.AudioAttribute2MP3ID3TagMapper;
 import de.kobich.audiosolutions.core.service.data.AudioDataService;
+import de.kobich.audiosolutions.core.service.mp3.id3.MP3ID3TagType;
 import de.kobich.audiosolutions.frontend.audio.editor.audiocollection.AudioCollectionEditor;
 import de.kobich.audiosolutions.frontend.common.listener.ActionType;
 import de.kobich.audiosolutions.frontend.common.listener.EventSupport;
@@ -44,48 +52,55 @@ public class SetAudioDataByID3TagsAction extends AbstractHandler {
 		if (fileDescriptors.isEmpty()) {
 			return null;
 		}
-
-		List<RunningState> states = Arrays.asList(RunningState.WORKER_1, RunningState.UI_2);
-		JFaceThreadRunner runner = new JFaceThreadRunner("Set Audio Data by ID3 Tags", window.getShell(), states) {
-
-			@Override
-			protected void run(RunningState state) throws Exception {
-				switch (state) {
-				case WORKER_1:
-					IServiceProgressMonitor progressMonitor = super.getProgressMonitor();
-					try {
-						progressMonitor.beginTask(new ProgressData("Set Audio Data by ID3 tags...", fileDescriptors.size()));
-
-						AudioDataService audioDataService = AudioSolutions.getService(AudioDataService.class);
-						audioDataService.addAudioDataByID3Tags(fileDescriptors, progressMonitor);
-					}
-					finally {
-						progressMonitor.endTask(new ProgressData("Audio data successfully added"));
-					}
-
-					break;
-				case UI_2:
-					UIEvent uiEvent = new UIEvent(ActionType.AUDIO_DATA, audioCollectionEditor);
-					uiEvent.getEditorDelta().getUpdateItems().addAll(fileDescriptors);
-					EventSupport.INSTANCE.fireEvent(uiEvent);
-
-					StatusLineUtils.setStatusLineMessage(audioCollectionEditor, "Audio data set", false);
-					break;
-				case UI_ERROR:
-					if (super.getProgressMonitor().isCanceled()) {
-						return;
-					}
-					Exception e = super.getException();
-					logger.error(e.getMessage(), e);
-					MessageDialog.openError(super.getParent(), super.getName(), "Operation failed: \n" + e.getMessage());
-					break;
-				default:
-					break;
-				}
-			}
-		};
-		runner.runProgressMonitorDialog(true, true);
 		
+		final ListSelectionDialog dialog = ListSelectionDialog.of(AudioAttribute2MP3ID3TagMapper.getInstance().getVariables())
+				.title("Set Audio Data By ID3 Tags")
+				.contentProvider(new ArrayContentProvider())
+				.labelProvider(LabelProvider.createTextProvider(o -> ((MP3ID3TagType) o).getLabel())).message("Select ID3 tags you want to apply:").create(window.getShell());
+		dialog.setInitialElementSelections(new ArrayList<>(AudioAttribute2MP3ID3TagMapper.getInstance().getVariables()));
+		int status = dialog.open();
+		if (status == IDialogConstants.OK_ID) {
+			final Set<MP3ID3TagType> selectedTags = Arrays.asList(dialog.getResult()).stream().map(r -> (MP3ID3TagType) r).collect(Collectors.toSet());
+			JFaceThreadRunner runner = new JFaceThreadRunner("Set Audio Data by ID3 Tags", window.getShell(), List.of(RunningState.WORKER_1, RunningState.UI_2)) {
+	
+				@Override
+				protected void run(RunningState state) throws Exception {
+					switch (state) {
+					case WORKER_1:
+						IServiceProgressMonitor progressMonitor = super.getProgressMonitor();
+						try {
+							progressMonitor.beginTask(new ProgressData("Set Audio Data by ID3 tags...", fileDescriptors.size()));
+	
+							AudioDataService audioDataService = AudioSolutions.getService(AudioDataService.class);
+							audioDataService.addAudioDataByID3Tags(fileDescriptors, selectedTags, progressMonitor);
+						}
+						finally {
+							progressMonitor.endTask(new ProgressData("Audio data successfully added"));
+						}
+	
+						break;
+					case UI_2:
+						UIEvent uiEvent = new UIEvent(ActionType.AUDIO_DATA, audioCollectionEditor);
+						uiEvent.getEditorDelta().getUpdateItems().addAll(fileDescriptors);
+						EventSupport.INSTANCE.fireEvent(uiEvent);
+	
+						StatusLineUtils.setStatusLineMessage(audioCollectionEditor, "Audio data set", false);
+						break;
+					case UI_ERROR:
+						if (super.getProgressMonitor().isCanceled()) {
+							return;
+						}
+						Exception e = super.getException();
+						logger.error(e.getMessage(), e);
+						MessageDialog.openError(super.getParent(), super.getName(), "Operation failed: \n" + e.getMessage());
+						break;
+					default:
+						break;
+					}
+				}
+			};
+			runner.runProgressMonitorDialog(true, true);
+		}
 		return null;
 	}
 
