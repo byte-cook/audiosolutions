57abda891627479077a6ba9b2200fa365a011f3d
bytecook <bytecook@t-online.de>
2024-04-25 21:07:22 +0200

    save last state

--- src/de/kobich/audiosolutions/frontend/audio/editor/playlist/action/AddFilesToPlaylistAction.java
+++ src/de/kobich/audiosolutions/frontend/audio/editor/playlist/action/AddFilesToPlaylistAction.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.ui.IEditorPart;
@@ -16,12 +17,16 @@ import org.eclipse.ui.handlers.HandlerUtil;
 
 import de.kobich.audiosolutions.core.service.playlist.EditablePlaylistFile;
 import de.kobich.audiosolutions.core.service.playlist.EditablePlaylistFolder;
+import de.kobich.audiosolutions.frontend.Activator;
 import de.kobich.audiosolutions.frontend.audio.editor.playlist.PlaylistEditor;
 import de.kobich.commons.type.Wrapper;
 import de.kobich.commons.ui.jface.JFaceExec;
+import de.kobich.commons.ui.jface.MementoUtils;
+import de.kobich.commons.ui.memento.IMementoItem;
 
 public class AddFilesToPlaylistAction extends AbstractHandler {
 	public static final String ID = "de.kobich.audiosolutions.commands.editor.addFilesToPlaylist";
+	private static final String STATE_DIR = "currentDir";
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
@@ -29,9 +34,19 @@ public class AddFilesToPlaylistAction extends AbstractHandler {
 		IEditorPart activeEditor = window.getActivePage().getActiveEditor();
 		
 		if (activeEditor instanceof PlaylistEditor playlistEditor) {
+			IDialogSettings dialogSettings = Activator.getDefault().getDialogSettings();
+			IMementoItem item = MementoUtils.getMementoItemToSave(dialogSettings, ID);
+			String currentDir = item.getString(STATE_DIR, System.getProperty("user.home"));
+			
 			FileDialog dialog = new FileDialog(window.getShell(), SWT.OPEN | SWT.MULTI);
+			dialog.setFilterPath(currentDir);
 			if (dialog.open() != null) {
 				final Set<File> selectedFiles = Arrays.asList(dialog.getFileNames()).stream().map(f -> new File(dialog.getFilterPath(), f)).collect(Collectors.toSet());
+				if (selectedFiles.isEmpty()) {
+					return null;
+				}
+				item.putString(STATE_DIR, selectedFiles.iterator().next().getParent());
+
 				Wrapper<EditablePlaylistFolder> folder = Wrapper.empty();
 				Wrapper<Set<EditablePlaylistFile>> addedFiles = Wrapper.empty();
 				JFaceExec.builder(window.getShell(), "Add Files To Playlist")
--- src/de/kobich/audiosolutions/frontend/audio/editor/playlist/action/CopyFilesToDirectoryAction.java
+++ src/de/kobich/audiosolutions/frontend/audio/editor/playlist/action/CopyFilesToDirectoryAction.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.ui.IEditorPart;
@@ -17,14 +18,18 @@ import org.eclipse.ui.handlers.HandlerUtil;
 import de.kobich.audiosolutions.core.AudioSolutions;
 import de.kobich.audiosolutions.core.service.playlist.EditablePlaylistFile;
 import de.kobich.audiosolutions.core.service.playlist.PlaylistService;
+import de.kobich.audiosolutions.frontend.Activator;
 import de.kobich.audiosolutions.frontend.audio.editor.playlist.PlaylistEditor;
 import de.kobich.audiosolutions.frontend.common.ui.FileResultDialog;
 import de.kobich.commons.type.Wrapper;
 import de.kobich.commons.ui.jface.JFaceExec;
+import de.kobich.commons.ui.jface.MementoUtils;
+import de.kobich.commons.ui.memento.IMementoItem;
 import de.kobich.component.file.FileResult;
 
 public class CopyFilesToDirectoryAction extends AbstractHandler {
 	public static final String ID = "de.kobich.audiosolutions.commands.editor.copyFilesToDirectory";
+	private static final String STATE_DIR = "currentDir";
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
@@ -37,10 +42,17 @@ public class CopyFilesToDirectoryAction extends AbstractHandler {
 				files = playlistEditor.getPlaylist().getAllFiles();
 			}
 			
+			IDialogSettings dialogSettings = Activator.getDefault().getDialogSettings();
+			IMementoItem item = MementoUtils.getMementoItemToSave(dialogSettings, ID);
+			String currentDir = item.getString(STATE_DIR, System.getProperty("user.home"));
+			
 			DirectoryDialog dialog = new DirectoryDialog(window.getShell(), SWT.SAVE);
 			dialog.setText("Select Target Directory");
+			dialog.setFilterPath(currentDir);
 			String selectedPath = dialog.open();
 			if (selectedPath != null) {
+				item.putString(STATE_DIR, selectedPath);
+				
 				final File targetDir = new File(selectedPath);
 				final Set<EditablePlaylistFile> filesFinal = new HashSet<>(files);
 				final Wrapper<FileResult> fileResult = Wrapper.empty();
@@ -58,10 +70,9 @@ public class CopyFilesToDirectoryAction extends AbstractHandler {
 						});
 					})
 					.exceptionalDialog("Copying files failed")
-					.runProgressMonitorDialog(true, false);
+					.runProgressMonitorDialog(true, true);
 			}
 		}
-		// TODO add AudiCollEditor
 		return null;
 	}
 
