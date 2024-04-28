7a34596dde4ac623eaa38b452f8140b6069751e3
bytecook <bytecook@t-online.de>
2024-04-28 18:43:26 +0200

    use gray foreground color if file name is not changed

--- src/de/kobich/audiosolutions/frontend/file/view/rename/ui/RenamingFilesPreviewLabelProvider.java
+++ src/de/kobich/audiosolutions/frontend/file/view/rename/ui/RenamingFilesPreviewLabelProvider.java
@@ -1,9 +1,13 @@
 package de.kobich.audiosolutions.frontend.file.view.rename.ui;
 
 import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.ITableColorProvider;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
 
 import de.kobich.audiosolutions.frontend.file.view.rename.RenameFilesView;
 import de.kobich.commons.ui.jface.table.ViewerColumn;
@@ -12,7 +16,7 @@ import de.kobich.component.file.descriptor.RenameFileDescriptor;
 /**
  * Label provider for renaming files view.
  */
-public class RenamingFilesPreviewLabelProvider extends LabelProvider implements ITableLabelProvider {
+public class RenamingFilesPreviewLabelProvider extends LabelProvider implements ITableLabelProvider, ITableColorProvider {
 	public RenamingFilesPreviewLabelProvider() {}
 
 	@Override
@@ -35,16 +39,38 @@ public class RenamingFilesPreviewLabelProvider extends LabelProvider implements 
 		return null;
 	}
 
+	@Override
 	public void addListener(ILabelProviderListener arg0) {
 	}
 
+	@Override
 	public void dispose() {
 	}
 
+	@Override
 	public boolean isLabelProperty(Object arg0, String arg1) {
 		return false;
 	}
 
+	@Override
 	public void removeListener(ILabelProviderListener arg0) {
+	}
+
+	@Override
+	public Color getForeground(Object element, int columnIndex) {
+		if (element instanceof RenameFileDescriptor renameFileDesc) {
+			ViewerColumn column = RenameFilesView.COLUMNS.getByIndex(columnIndex);
+			if (RenameFilesView.COLUMN_NEW_NAME.equals(column)) {
+				if (renameFileDesc.getOriginalName().equals(renameFileDesc.getName())) {
+					return Display.getCurrent().getSystemColor(SWT.COLOR_GRAY);
+				}
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public Color getBackground(Object element, int columnIndex) {
+		return null;
 	}
 }
