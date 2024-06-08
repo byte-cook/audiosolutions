7df2614870baa778124f20ab618bf9690f3f19f5
bytecook <bytecook@t-online.de>
2024-06-08 18:48:07 +0200

    fix: filter folders as well

--- src/de/kobich/audiosolutions/frontend/audio/editor/playlist/PlaylistEditorViewerFilter.java
+++ src/de/kobich/audiosolutions/frontend/audio/editor/playlist/PlaylistEditorViewerFilter.java
@@ -1,8 +1,5 @@
 package de.kobich.audiosolutions.frontend.audio.editor.playlist;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOCase;
 import org.apache.commons.lang3.StringUtils;
@@ -26,28 +23,36 @@ public class PlaylistEditorViewerFilter extends ViewerFilter {
 			return true;
 		}
 		
-		List<EditablePlaylistFile> files = new ArrayList<>();
 		if (element instanceof EditablePlaylistFile file) {
-			files.add(file);
-		}
-		else if (element instanceof EditablePlaylistFolder folder) {
-			files.addAll(folder.getFiles());
-		}
-		
-		// perform filtering
-		for (EditablePlaylistFile file : files) {
 			if (select(file)) {
 				return true;
 			}
 		}
+		else if (element instanceof EditablePlaylistFolder folder) {
+			if (select(folder)) {
+				return true;
+			}
+			
+			for (EditablePlaylistFile file : folder.getFiles()) {
+				if (select(file)) {
+					return true;
+				}
+			}
+		}
 		return false;
+	}
+	
+	public boolean select(EditablePlaylistFolder folder) {
+		if (StringUtils.isEmpty(search)) {
+			return true;
+		}
+		return FilenameUtils.wildcardMatch(folder.getPath(), search, IOCase.INSENSITIVE);
 	}
 	
 	public boolean select(EditablePlaylistFile file) {
 		if (StringUtils.isEmpty(search)) {
 			return true;
 		}
-		
 		return FilenameUtils.wildcardMatch(file.getName(), search, IOCase.INSENSITIVE) || FilenameUtils.wildcardMatch(file.getFile().getAbsolutePath(), search, IOCase.INSENSITIVE);
 	}
 	
